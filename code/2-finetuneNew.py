10cd6a54530954ec3b1b4184f66c1bdd379f355e
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2024-01-26 22:07:29 +0100

    facciamo il save anche dopo ctrl-c

--- code/2-finetuneNew.py
+++ code/2-finetuneNew.py
@@ -17,6 +17,7 @@ from transformers import CLIPTextModel
 import torch.nn.functional as F
 import clip as CLIP
 from einops import rearrange
+import signal
 # 1 Caricare il modello pretrained V
 # 2 Prendere labeledEEG gli facciamo l'encoder per ricavare la noise 
 # 3 aggiungiamo a questa noise una randomNoise
@@ -30,6 +31,7 @@ from einops import rearrange
 # 10 loss.backward()
 # 11 optimizer.step()
 # 11 optimizer.zero_grad()
+
 
 def main(config):
     ## 1 Caricare il modello pretrained
@@ -94,80 +96,73 @@ def main(config):
     unet,encoder, vae,scheduler,optimizer,projector1,clip_model,crop_transform =  accelerator.prepare(unet,encoder, vae,scheduler,optimizer,projector1,clip_model,crop_transform)
     ## For Epoch
     train_loss = 0.0
-    for epoch in range(0, config.num_epoch):
-        current_dateTime = datetime.datetime.now()
-        print("DataInizio: "+ str(current_dateTime))
-        print(f"Epoch: {epoch}")
-        for step, batch in enumerate(eeg_latents_dataset_train ):
-            eeg = batch["eeg"]
-            image = batch["image"]
-            with accelerator.accumulate(unet, encoder,projector1):
-                embeddings = encoder(eeg) #from torch.Size([128, 512]) to torch.Size([1, 128, 1024])
-                hidden_states = projector1(embeddings)
-                image_for_encode = change_shape_for_encode(image)
-                
-                # Convert images to latent space
-                latents = vae.encode(image_for_encode).latent_dist.sample() # need torch.Size([1, 3, 64, 64])
-                latents = latents * vae.config.scaling_factor
+    try:
+        for epoch in range(0, config.num_epoch):
+            current_dateTime = datetime.datetime.now()
+            print("DataInizio: "+ str(current_dateTime))
+            print(f"Epoch: {epoch}")
+            for step, batch in enumerate(eeg_latents_dataset_train ):
+                eeg = batch["eeg"]
+                image = batch["image"]
+                with accelerator.accumulate(unet, encoder,projector1):
+                    embeddings = encoder(eeg) #from torch.Size([128, 512]) to torch.Size([1, 128, 1024])
+                    hidden_states = projector1(embeddings)
+                    image_for_encode = change_shape_for_encode(image)
+                    
+                    # Convert images to latent space
+                    latents = vae.encode(image_for_encode).latent_dist.sample() # need torch.Size([1, 3, 64, 64])
+                    latents = latents * vae.config.scaling_factor
 
-                # 3 aggiungiamo a questa noise una randomNoise
-                # Sample noise that we'll add to the latents
-                noise = torch.randn_like(latents).to(accelerator.device)
+                    # 3 aggiungiamo a questa noise una randomNoise
+                    # Sample noise that we'll add to the latents
+                    noise = torch.randn_like(latents).to(accelerator.device)
 
-                #bsz = latents.shape[0]
-                # Sample a random timestep for each image
-                timesteps = torch.randint(0, 1000, (1,), device=latents.device)
-                timesteps = timesteps.long()
+                    #bsz = latents.shape[0]
+                    # Sample a random timestep for each image
+                    timesteps = torch.randint(0, 1000, (1,), device=latents.device)
+                    timesteps = timesteps.long()
 
-                # Add noise to the latents according to the noise magnitude at each timestep
-                # (this is the forward diffusion process)
+                    # Add noise to the latents according to the noise magnitude at each timestep
+                    # (this is the forward diffusion process)
+                    
+                    noisy_latents = scheduler.add_noise(latents, noise, timesteps)
                 
-                noisy_latents = scheduler.add_noise(latents, noise, timesteps)
-              
-                # 4 passare tutto alla uNET per poi calcolarci la loss A
-                # Predict the noise residual and compute loss
-                model_pred = unet(noisy_latents, timesteps, hidden_states, return_dict=False)[0] #RuntimeError: mat1 and mat2 shapes cannot be multiplied (128x1024 and 768x320)
-                loss_unet = F.mse_loss(model_pred,noise,reduction="mean")
-                
-                # 5 prendo l'immagine dell'egg relativo, lo croppo    
-                image_cropped= crop_transform(image_for_encode)
-                # 6 uso l'encoder di clip per trovare l'embedding dell'immagine
-                image_encoded = clip_model.encode_image(image_cropped)
-                
-                # 7 utilizzo una projection per poter confrontare i due embedding trovati
-                projection = projection_layer(hidden_states)
-                # 8 calcolo la cosine-similarity tra i due embeddign, ricevendo un altra loss B
+                    # 4 passare tutto alla uNET per poi calcolarci la loss A
+                    # Predict the noise residual and compute loss
+                    model_pred = unet(noisy_latents, timesteps, hidden_states, return_dict=False)[0] #RuntimeError: mat1 and mat2 shapes cannot be multiplied (128x1024 and 768x320)
+                    loss_unet = F.mse_loss(model_pred,noise,reduction="mean")
+                    
+                    # 5 prendo l'immagine dell'egg relativo, lo croppo    
+                    image_cropped= crop_transform(image_for_encode)
+                    # 6 uso l'encoder di clip per trovare l'embedding dell'immagine
+                    image_encoded = clip_model.encode_image(image_cropped)
+                    
+                    # 7 utilizzo una projection per poter confrontare i due embedding trovati
+                    projection = projection_layer(hidden_states)
+                    # 8 calcolo la cosine-similarity tra i due embeddign, ricevendo un altra loss B
 
-                # hidden_states.shape torch.Size([1, 128, 768])
-                # projection   .shape torch.Size([1, 128, 512])
-                # image_encoded.shape torch.Size([1, 512])
-                loss_clip = 1 - F.cosine_similarity(image_encoded,projection)
-                
-                # 9 somme le due loss (A + B)
-                total_loss = loss_unet + loss_clip
-                if (math.isfinite(total_loss.item()) == False): exit(1)
+                    # hidden_states.shape torch.Size([1, 128, 768])
+                    # projection   .shape torch.Size([1, 128, 512])
+                    # image_encoded.shape torch.Size([1, 512])
+                    loss_clip = 1 - F.cosine_similarity(image_encoded,projection)
+                    
+                    # 9 somme le due loss (A + B)
+                    total_loss = loss_unet + loss_clip
+                    if (math.isfinite(total_loss.item()) == False): exit(1)
 
-                print(str(total_loss.item()) + " Step: "+ str(step))
-                
-                accelerator.backward(total_loss)
-                optimizer.step()
-                optimizer.zero_grad()
-        current_dateTime = datetime.datetime.now()
-        print("DataFine: "+ str(current_dateTime))
-        torch.save(
-                    {
-                       'unet_state_dict': unet.state_dict(),
-                        'egg_encoder_state_dict': encoder.state_dict(),
-                        'vae_state_dict': vae.state_dict(),
-                        # 'scheduler_state_dict': scheduler.state_dict(),
-                        'clip_model_state_dict': clip_model.state_dict(),
-                        'projector1': projector1.state_dict(),
-                        'config': config,
-                        'state': torch.random.get_rng_state()
+                    print(str(total_loss.item()) + " Step: "+ str(step))
+                    
+                    accelerator.backward(total_loss)
+                    optimizer.step()
+                    optimizer.zero_grad()
+            current_dateTime = datetime.datetime.now()
+            print("DataFine: "+ str(current_dateTime))
+            save_model(unet, encoder, vae, clip_model, projector1, config, config.output_path, epoch)     
 
-                    },
-                    os.path.join(config.output_path, 'checkpoint.pth'))                      
-                
+    except KeyboardInterrupt:
+        print("Training interrupted. Saving model...")
+        save_model(unet, encoder, vae, clip_model, projector1, config, config.output_path, epoch=None)
+        exit(0)
                 
 class ProjectionLayerH(nn.Module):
     def __init__(self, input_size, output_size):
@@ -181,18 +176,19 @@ class ProjectionLayerH(nn.Module):
         x = self.projection(x)
         return x
 
-
-
-def save_model(config, epoch, model, optimizer, loss_scaler, checkpoint_paths):
-    os.makedirs(checkpoint_paths, exist_ok=True)
-    to_save = {
-        'model': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'epoch': epoch,
-        'scaler': loss_scaler.state_dict(),
-        'config': config,
-    }
-    torch.save(to_save, os.path.join(checkpoint_paths, 'checkpoint.pth'))
+def save_model(unet, encoder, vae, clip_model, projector1, config, output_path, epoch=None):
+    torch.save(
+        {
+            'unet_state_dict': unet.state_dict(),
+            'egg_encoder_state_dict': encoder.state_dict(),
+            'vae_state_dict': vae.state_dict(),
+            'clip_model_state_dict': clip_model.state_dict(),
+            'projector1': projector1.state_dict(),
+            'config': config,
+            'state': torch.random.get_rng_state()
+        },
+        os.path.join(output_path, f'checkpoint_epoch_{epoch}.pth') if epoch is not None else os.path.join(output_path, 'manual_checkpoint.pth')
+    )
     
 def prepareOutputPath(config):
     output_path = os.path.join(
@@ -266,6 +262,7 @@ def get_args_parser():
 
 def change_shape_for_encode(image):
     return rearrange(image, "h w c-> c h w").unsqueeze(0).to(dtype=torch.float32)
+
 
 if __name__ == "__main__":
     args = get_args_parser()
