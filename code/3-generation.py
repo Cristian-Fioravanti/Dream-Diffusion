200d3bfe684c9e96dfad7ec85951dda2edb35928
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2024-02-12 15:30:27 +0100

    gen img

--- code/3-generation.py
+++ code/3-generation.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from encoder.eegEncoder import eegEncoder
 from config import Config_Generative_Model, Config_MBM_EEG
 from dataset import create_EEG_dataset
-from diffusers import AutoencoderKL, DDPMScheduler,LMSDiscreteScheduler, StableDiffusionPipeline, UNet2DConditionModel
-from diffusers import LMSDiscreteScheduler
+from diffusers import AutoencoderKL, DDPMScheduler, LMSDiscreteScheduler, StableDiffusionPipeline, UNet2DConditionModel
+from diffusers import PNDMScheduler
 
 import torch
 import torch.nn as nn
@@ -24,7 +24,7 @@ import clip as CLIP
 from einops import rearrange
 from tqdm.auto import tqdm
 # 1 Caricare il modello pretrained V
-# 2 Prendere labeledEEG gli facciamo l'encoder per ricavare la noise 
+# 2 Prendere labeledEEG gli facciamo l'encoder per ricavare la noise
 # 3 aggiungiamo a questa noise una randomNoise
 # 4 passare tutto alla uNET per poi calcolarci la loss A
 
@@ -37,20 +37,22 @@ from tqdm.auto import tqdm
 # 11 optimizer.step()
 # 11 optimizer.zero_grad()
 
+
 def main(config):
     logging_dir = os.path.join(config.output_path, "accLog/")
-    ## 1 Caricare il modello pretrained
-    pretrain_model = torch.load('..\dreamdiffusion\pretrains\eeg_pretrain\checkpoint.pth', map_location="cpu")
+    # 1 Caricare il modello pretrained
+    pretrain_model = torch.load(
+        '..\dreamdiffusion\pretrains\eeg_pretrain\checkpoint.pth', map_location="cpu")
     metafile_config = pretrain_model['config']
-    
-    
-    ## 2 Prendere labeledEEG gli facciamo l'encoder per ricavare la noise
+    device = "cpu"
+
+    # 2 Prendere labeledEEG gli facciamo l'encoder per ricavare la noise
     # Training image transformations
     img_transform_train = transforms.Compose(
         [
             ut.normalize,
             transforms.Resize((512, 512)),
-            transforms.RandomCrop(size=(64, 64)),
+            transforms.RandomCrop(size=(512, 512)),
             transforms.Normalize([0.5], [0.5]),
             # transforms.Resize((512, 512)),
             ut.channel_last,
@@ -61,8 +63,8 @@ def main(config):
     img_transform_test = transforms.Compose(
         [ut.normalize, transforms.Resize((512, 512)), ut.channel_last]
     )
-    crop_transform = transforms.CenterCrop((224,224))
-        
+    crop_transform = transforms.CenterCrop((224, 224))
+
     eeg_latents_dataset_train, eeg_latents_dataset_test = create_EEG_dataset(
         eeg_signals_path=config.eeg_signals_path,
         splits_path=config.splits_path,
@@ -70,8 +72,9 @@ def main(config):
         subject=config.subject,
     )
     data_len_eeg = eeg_latents_dataset_train.data_len
-    accelerator_project_config = ProjectConfiguration(project_dir=config.output_path, logging_dir=logging_dir)
-    
+    accelerator_project_config = ProjectConfiguration(
+        project_dir=config.output_path, logging_dir=logging_dir)
+
     accelerator = Accelerator(
         gradient_accumulation_steps=1,
         mixed_precision=None,
@@ -80,18 +83,21 @@ def main(config):
     )
     # Carica il modello dalla cartella specificata
     folder_path = "../dreamdiffusion/exps/results/generation/"
-    model_name = "checkpoint_epoch_0.pth"
+    model_name = "checkpoint_epoch_19.pth"
     model_path = Path(folder_path) / model_name
     generative_model = torch.load(model_path)
 
-    #Carico Encoder
+    # Carico Encoder
     encoder = eegEncoder(time_len=data_len_eeg, patch_size=metafile_config.patch_size, embed_dim=metafile_config.embed_dim,
-                        depth=metafile_config.depth, num_heads=metafile_config.num_heads,mlp_ratio=metafile_config.mlp_ratio)
-   
-    scheduler = DDPMScheduler.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="scheduler")
-    unet = UNet2DConditionModel.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="unet")
-    vae = AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="vae")
-    clip_model, preprocess = CLIP.load(name="ViT-L/14", device="cpu")    
+                         depth=metafile_config.depth, num_heads=metafile_config.num_heads, mlp_ratio=metafile_config.mlp_ratio)
+
+    scheduler = PNDMScheduler.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", subfolder="scheduler")
+    unet = UNet2DConditionModel.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", subfolder="unet")
+    vae = AutoencoderKL.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", subfolder="vae")
+    clip_model, preprocess = CLIP.load(name="ViT-L/14", device="cpu")
     projector1 = ProjectionLayerEmbedding(128*metafile_config.embed_dim, 59136)
 
     unet.load_state_dict(generative_model['unet_state_dict'])
@@ -99,47 +105,55 @@ def main(config):
     vae.load_state_dict(generative_model['vae_state_dict'])
     clip_model.load_state_dict(generative_model['clip_model_state_dict'])
     projector1.load_state_dict(generative_model['projector1'])
-        
-    for step, batch in enumerate(eeg_latents_dataset_test ):
+
+    for step, batch in enumerate(eeg_latents_dataset_train):
         eeg = batch["eeg"]
         image = batch["image"]
-        embeddings = encoder(eeg) 
+
+        image_real = Image.fromarray(
+            ((image / 2 + 0.5).clamp(0, 1) * 255).numpy().astype('uint8'))
+        image_real_path = os.path.join(config.output_path,
+                                       f'./real{step}.png')
+        image_real.save(image_real_path)
+
+        embeddings = encoder(eeg)
         hidden_states = projector1(embeddings)
         image_for_encode = change_shape_for_encode(image)
 
-        latents = vae.encode(image_for_encode).latent_dist.sample() 
+        latents = vae.encode(image_for_encode).latent_dist.sample()
         latents = latents * vae.config.scaling_factor
 
         input = torch.randn_like(latents).to(accelerator.device)
 
         # timesteps = torch.randint(0, 1000, (1,), device=latents.device)
-        timesteps = 999
+        timesteps = 20
         scheduler.set_timesteps(timesteps)
         for t in tqdm(scheduler.timesteps):
             with torch.no_grad():
-                noisy_residual = unet(input, t, hidden_states.unsqueeze(0), return_dict=False)[0]
-                prev_noisy_sample = scheduler.step(noisy_residual, t, input).prev_sample
+                noisy_residual = unet(
+                    input, t, hidden_states.unsqueeze(0), return_dict=False)[0]
+                prev_noisy_sample = scheduler.step(
+                    noisy_residual, t, input).prev_sample
                 input = prev_noisy_sample
 
         input = 1 / 0.18215 * input
         with torch.no_grad():
             image_gen = vae.decode(input).sample
-        
+
         image_gen = (image_gen / 2 + 0.5).clamp(0, 1)
         image_gen = image_gen.cpu().permute(0, 2, 3, 1).numpy()[0]
         image_gen = Image.fromarray((image_gen * 255).round().astype("uint8"))
-        
-        image_gen_path = os.path.join(config.output_path, 
-                        f'./test{step}.png')
-        
+
+        image_gen_path = os.path.join(config.output_path,
+                                      f'./test{step}.png')
+
         image_gen.save(image_gen_path)
 
-                
-                
+
 class ProjectionLayerEmbedding(nn.Module):
     def __init__(self, input_size, output_size):
         super(ProjectionLayerEmbedding, self).__init__()
-      
+
         self.projection = nn.Linear(input_size, output_size)
 
     def forward(self, x):
@@ -147,9 +161,9 @@ class ProjectionLayerEmbedding(nn.Module):
         x = self.projection(x.flatten())
         # Apply the linear projection
 
-        return torch.reshape(x,(77,768))
+        return torch.reshape(x, (77, 768))
 
-    
+
 def prepareOutputPath(config):
     output_path = os.path.join(
         config.output_path,
@@ -220,8 +234,10 @@ def get_args_parser():
 
     return parser
 
+
 def change_shape_for_encode(image):
     return rearrange(image, "h w c-> c h w").unsqueeze(0).to(dtype=torch.float32)
+
 
 if __name__ == "__main__":
     args = get_args_parser()
